eb8d6f824ba407b6d1fe1442dfc949ffff5f3cd5
Malak <malak.thabetx10x@gmail.com>
2026-01-14 16:10:02 +0800

    fixed ChooseInterestsActivity buttons,

--- app/src/main/java/com/example/knowly/HomePage.java
+++ app/src/main/java/com/example/knowly/HomePage.java
@@ -8,6 +8,6 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page);
+        setContentView(R.layout.home_page); // <-- change to your actual home xml filename
     }
 }
