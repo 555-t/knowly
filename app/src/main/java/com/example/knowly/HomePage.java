5e2f7c1b22018bacb5c3e131919f71e100ecef7b
kamtrbl <103358078+kamtrbl@users.noreply.github.com>
2026-01-14 21:56:33 +0800

    connected home_page to fragment_weeklyfeatured.xml
    (cherry picked from commit 3d7ae9f3670603df581996187d8370d16cfbac25)

--- app/src/main/java/com/example/knowly/HomePage.java
+++ app/src/main/java/com/example/knowly/HomePage.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View;
 import android.widget.ImageView;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.card.MaterialCardView;
 
@@ -23,6 +25,21 @@ public class HomePage extends AppCompatActivity {
             public void onClick(View v) {
                 // This starts the transition to the new page
                 Intent intent = new Intent(HomePage.this, CreatePostActivity.class);
+                startActivity(intent);
+            }
+        });
+        setContentView(R.layout.home_page);
+
+        // 1. Find the navWeekly view inside the included bottom_nav_bar
+        // Since it's included, it's part of the main view hierarchy
+        ImageView navWeekly = findViewById(R.id.navWeekly);
+
+        // 2. Set the click listener
+        navWeekly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 3. Create an Intent to start the Weekly Featured activity
+                Intent intent = new Intent(HomePage.this, WeeklyFeaturedActivity.class);
                 startActivity(intent);
             }
         });
